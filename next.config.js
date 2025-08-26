b94d055ad515363efe7e30c0d41c66ff7307bbac
Infant Elfrick Gnanasusairaj <infant.elfrick@gmail.com>
2025-08-25 22:25:24 -0500

    netlify toml for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App Router is now stable in Next.js 14, no experimental flag needed
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    loader: 'default',
+    path: ''
+  },
+  assetPrefix: '',
+  basePath: ''
 };
 
 module.exports = nextConfig;
